d423f106a48257807bd64a18fb6cf95ba12ffe42
Elmar465 <elmar465@gmail.com>
2025-01-30 15:50:34 -0500

    Added Put method and Delete Methods

--- src/main/java/com/example/resttemplateexample/api/ClaimApi.java
+++ src/main/java/com/example/resttemplateexample/api/ClaimApi.java
@@ -5,8 +5,10 @@ import com.example.resttemplateexample.clients.UserClient;
 import com.example.resttemplateexample.model.UserModel;
 import com.example.resttemplateexample.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
@@ -39,23 +41,27 @@ public class ClaimApi {
     return  userModel;
     }
 
+    //* i Commented feign client you can turn and change api request from interface
 
-    @PostMapping("/create")
-    public UserModel create(@RequestBody UserModel userModel) {
-        return userClient.createUser(userModel);
-    }
+//    @PostMapping("/create")
+//    public UserModel create(@RequestBody UserModel userModel) {
+//        return userClient.createUser(userModel);
+//    }
+
+    //* i Commented feign client you can turn and change api request from interface
+
+//    @PutMapping("/update")
+//    public UserModel update() {
+//        return userClient.updateUser(102,"Ali100@Gmail.com");
+//    }
 
 
-    @PutMapping("/update")
-    public UserModel update() {
-        return userClient.updateUser(102,"Ali100@Gmail.com");
-    }
+    //* i Commented feign client you can turn and change api request from interface
 
-
-    @DeleteMapping("/delete")
-    void delete() {
-        userClient.deleteUser(102);
-    }
+//    @DeleteMapping("/delete")
+//    void delete() {
+//        userClient.deleteUser(3);
+//    }
 
 
     @GetMapping("/getAllUsers")
@@ -63,17 +69,36 @@ public class ClaimApi {
         return userClient.getAllUser();
     }
 
-//
-//    @PostMapping("/add")
-//    public UserModel createUser(@RequestBody  UserModel userModel) {
-//        return webClient.post()
-//                .uri("http://localhost:8080/users/create")
-//                .body(Mono.just(userModel), UserModel.class)
-//                .retrieve()
-//                .bodyToMono(UserModel.class)
-//                .block(Duration.ofMillis(1000));
-//    }
-//    public String createUser(@RequestBody UserModel user) {
-//        return userService.createUser(user).getUserName();
-//    }
+
+    @PostMapping("/add")
+    public UserModel create(@RequestBody UserModel userModel) {
+        return  webClient.post()
+                .uri("http://localhost:8085/users/createUser")
+                .bodyValue(userModel)
+                .retrieve()
+                .bodyToMono(UserModel.class)
+                .block();
+    }
+
+    @PutMapping("/update")
+    public UserModel updateUser(UserModel model) {
+        return webClient
+                .put()
+                .uri("http://localhost:8085/users/updateAUser/1/Elmar565@gmail.com")
+                .bodyValue(model)
+                .retrieve()
+                .bodyToMono(UserModel.class)
+                .block();
+    }
+
+
+    @DeleteMapping("/delete")
+    public void deleteUser(){
+        webClient
+                .method(HttpMethod.DELETE)
+                .uri("http://localhost:8085/users/2")
+                .retrieve()
+                .bodyToMono(UserModel.class)
+                .block();
+    }
 }
